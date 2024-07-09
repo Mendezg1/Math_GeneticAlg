555f7bb38009c6dd68dc05c0797f0cc8a3ab009f
Mendezg1 <87867522+Mendezg1@users.noreply.github.com>
2024-07-08 18:29:35 -0600

    Final

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -2,13 +2,13 @@ import math, random
 import itertools
 
 def math_func(x):
-    return x * math.sin(10 * x) + 1 
+    return x * math.sin(10 * math.pi * x) + 1 
 
 
 population = {}
 
 # Generate Population
-for _ in range(1000):
+for _ in range(20):
     number = random.uniform(0,1)
     population[number] = math_func(number)
 
@@ -17,30 +17,36 @@ population = dict(sorted(population.items(), key=lambda item: item[1], reverse=T
 selected = round(len(population) * 0.25) + 1
 
 new_population = dict(itertools.islice(population.items(), selected))
-position = 0
 
 
-for _ in range(100):
+for _ in range(50):
     crossover = {}
-    str_number = str(new_population.items()[0])
+    max_k = list(new_population.keys())[0]
+    max_n = new_population[max_k]
     for key1, key2 in itertools.combinations(new_population.keys(), 2):
         #Crossover between key values
         new_key = (key1 + key2) / 2
-        new_value = math_func(new_key)
         
         #Mutation of highest value nth decimal
-        new_value_str = str(new_value)
-        dot_position = new_value_str.find('.')
-        pos = dot_position + position
-        new_value_str = new_value_str[:pos] + str_number + new_value_str[pos + 1:]
-        mutated_value = float(new_value_str)
+        choice = random.choice([0,1,2])
+        if choice == 1:
+            new_key += 0.005
+        elif choice == 2:
+            new_key -= 0.005
+            
+        new_value = math_func(new_key)
         
-        position += 1
-        crossover[new_key] = mutated_value
+        crossover[new_key] = new_value
     
     population = dict(sorted(crossover.items(), key=lambda item: item[1], reverse=True))
     selected = round(len(crossover) * 0.25) + 1
-    new_population = dict(itertools.islice(population.items(), selected))
+    new_population_1 = dict(itertools.islice(population.items(), selected))
+    if next(iter(new_population_1.values())) >= max_n:
+        new_population = new_population_1
+        max_k = list(new_population.keys())[0]
+        max_n = new_population[max_k]
+        
+print(f"El máximo encontrado es: f({max_k}) = {max_n}. \n")
     
 
 
